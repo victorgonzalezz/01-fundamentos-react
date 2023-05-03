5ec8d0970ebbf6272cc67a1a0f631d3db79bc3d1
Victor Gonzalez <vitorpcg@gmail.com>
2023-05-03 17:50:26 +0100

    Update Post.jsx
    Delete Comment done

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,27 +8,43 @@ import { useState } from 'react';
 
 
 export function Post({ author, publishedAt, content }) {
-  const [comments, setComments] = useState([
-    1,
-    2,
-  ]);
+  const [comments, setComments] = useState(["Post muito bacana, hein?!"]);
+
+  const [newCommentText, setNewCommentText] = useState("");
 
   const publishedDateFormatted = format(
     publishedAt,
-    "'Dia' dd 'de' MMMM', às ' HH:mm'h'", {
-    locale: pt
-  }
+    "'Dia' dd 'de' MMMM', às ' HH:mm'h'",
+    {
+      locale: pt,
+    }
   );
 
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: pt,
     addSuffix: true,
   });
-  
-  function handleCreateNewComment() {
+
+  //Declarativa
+  function handleCreateNewComment(event) {
     event.preventDefault();
-    setComments([ ...comments, comments.length + 1])
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText("");
   }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
+  }
+
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(comment => {
+      return comment !== commentToDelete
+    })
+    setComments(commentsWithoutDeletedOne);
+  }//bloco de código para iniciar os estudos de filter amanha no GPT
+
+
 
   return (
     <article className={styles.post}>
@@ -52,10 +68,10 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === "link") {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="#">{line.content}</a>
               </p>
             );
@@ -66,15 +82,26 @@ export function Post({ author, publishedAt, content }) {
       <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
-        <textarea placeholder="Deixe um comentário" />
+        <textarea
+          name="comment"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+          placeholder="Deixe um comentário"
+        />
         <footer>
           <button type="submit">Publicar</button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
-        {comments.map((comment) => {
-          return <Comment />;
+        {comments.map(comment => {
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          );
         })}
       </div>
     </article>
